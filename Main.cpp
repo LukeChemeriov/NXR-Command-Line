61b7e86cb832f1ecab8dba25cadbf3a11b3f735a
Luke Chemeriov <87149262+LukeChemeriov@users.noreply.github.com>
2021-08-22 15:48:25 +0100

    Update Main.cpp
    Added contents, including main, and the command loop

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,21 @@
+/* Main.cpp
+
+"...where all the good stuff's goin' on" ~ John Hammond, literally every video ever
+
+*/
+
+/* A preprocessor statement. Just the one. */
+#include <iostream>
+#include "/functions/functions.h"
+
+int main() { /* the infamous main */
+    /* Main loop */
+    while (true) {
+        /* Get the user's command */
+        std::getline(std::cin, command);
+        /* Test it */
+        Test(command);
+
+    }
+}
 
